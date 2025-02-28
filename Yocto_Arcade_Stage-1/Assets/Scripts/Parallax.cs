0daed9abaed22b51b8d3c8364d2d26634d47cf67
URLL-spec <dobri.txt@gmail.com>
2025-02-28 12:38:24 +0500

    Update-1
    - Creation of basic movement mechanics
    - Testing frame-by-frame animation
    - Testing the Camera settings
    
    - Создание базовой механики перемещения
    - Тестирование покадровой анимации
    - Тестирование настроек Камеры

---
+++ Yocto_Arcade_Stage-1/Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public GameObject cam;
+
+    float stratRox;
+    public float parallax;
+    float startPosX;
+
+    void Start()
+    {
+        startPosX = transform.position.x;
+    }
+
+    void Update()
+    {
+        float distX = (cam.transform.position.x * (1 - parallax));
+        transform.position = new Vector3(startPosX + distX, transform.position.y, transform.position.z);
+    }
+}
